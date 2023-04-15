f4e842d15be2350ebd5910abe25eef9dc39e9f0f
Luis <luisfernandosantaospina@gmail.com>
2023-04-15 10:03:33 -0500

    Add postgres

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "")
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///task.sqlite'
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://' + \
-    #                           os.environ.get('PGSQL_USER') + ':' + \
-    #                           os.environ.get('PGSQL_PASSWORD') + '@' + \
-    #                           os.environ.get('PGSQL_HOST') + ':' + \
-    #                           os.environ.get('PGSQL_PORT') + '/' + \
-    #                           os.environ.get('PGSQL_DATABASE')
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///task.sqlite'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://' + \
+                              os.environ.get('PGSQL_USER') + ':' + \
+                              os.environ.get('PGSQL_PASSWORD') + '@' + \
+                              os.environ.get('PGSQL_HOST') + ':' + \
+                              os.environ.get('PGSQL_PORT') + '/' + \
+                              os.environ.get('PGSQL_DATABASE')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PROPAGATE_EXCEPTIONS = True
     ACCESS_EXPIRES = timedelta(hours=2)
