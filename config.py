02f8be3e425b057cefc8d94f5e8010ccdf942c84
Luis <luisfernandosantaospina@gmail.com>
2023-04-15 08:10:59 -0500

    Add env and sqlite

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "")
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///D:///task.sqlite'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///task.sqlite'
     # SQLALCHEMY_DATABASE_URI = 'postgresql://' + \
     #                           os.environ.get('PGSQL_USER') + ':' + \
     #                           os.environ.get('PGSQL_PASSWORD') + '@' + \
