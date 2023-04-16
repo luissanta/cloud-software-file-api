3bcbadf06706de779fd7bb3a2a1d10b4bee91b4c
Luis <luisfernandosantaospina@gmail.com>
2023-04-16 09:57:06 -0500

    Add git prod

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "")
     JWT_SECRET_KEY = os.environ.get("SECRET_KEY", "")
     SQLALCHEMY_DATABASE_URI = 'postgresql://' + \
                               os.environ.get('PGSQL_USER') + ':' + \
