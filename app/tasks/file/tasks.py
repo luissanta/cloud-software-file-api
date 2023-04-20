c9ec2809765a705bca7227a828a8aec7b7a54dab
Milton Beltran <mj.beltran37@uniandes.edu.co>
2023-04-20 13:43:24 -0500

    add save in nfs

--- app/tasks/file/tasks.py
+++ app/tasks/file/tasks.py
@@ -1,15 +1,17 @@
+from app.repositories.file.manager_repository import FileManager, NetworkFileStorage
 from celery_app import celery
 from app.repositories.file import get_detail_by_id, update
 from app.models import File
 from app.factories import CompressorFactory
 from app.enums.file import ConverterStatusEnum
 
+file_manager = FileManager(NetworkFileStorage())
 
 @celery.task(name='converter.request')
 def converter_request(task_id: str, file_id: int, new_format: str) -> None:
     status = ConverterStatusEnum.PROCESSED.value
     try:
-        fetched_file = get_detail_by_id(File(id=file_id))
+        fetched_file = file_manager.get_file(File(id=file_id))
 
         compressor_factory = CompressorFactory()
         compresor_type = compressor_factory.get_compressor(new_format.upper())
@@ -17,9 +19,10 @@ def converter_request(task_id: str, file_id: int, new_format: str) -> None:
             fetched_file.original_data,
             fetched_file.original_name
         )
-        update(File(id=file_id, compressed_data=file_compress_data, compressed_name=file_compress_name))
+        file_manager.save_file(new_format,File(id=file_id, compressed_data=file_compress_data, compressed_name=file_compress_name, temporal_name=fetched_file.temporal_name))        
 
-    except:
+    except Exception as e:
+        print(f"Detail error: {e}")
         status = ConverterStatusEnum.FAILED.value
 
     finally:
