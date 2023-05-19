381596215c65fc88554746f288adaacdc5bca3b3
felipe <lf.garciar1@uniandes.edu.co>
2023-05-18 21:45:29 -0500

    add

--- app/routes/api.py
+++ app/routes/api.py
@@ -14,12 +14,13 @@ file_converter = FileConverter()
 
 @api_routes.route('/files/process', methods=['POST'])
 def create_task():
-    data = json.loads(request.data)
-    message =json.loads(base64.b64decode(data['message']['data']).decode('utf-8'))
     try:
+        data = json.loads(request.data)
+        message =json.loads(base64.b64decode(data['message']['data']).decode('utf-8'))
         file_converter.converter_request(message["task_id"], message["url"], message["new_format"])
         return 200
     except Exception as e:
         logger.error("error processing message: error {error}".format(error=e))
-        return 500
+        return {"error": e}, 501
+
 
