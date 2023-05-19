c50b68926540b5230984e7d1faa471d8c641204e
felipe <lf.garciar1@uniandes.edu.co>
2023-05-18 23:02:20 -0500

    version estable

--- app/routes/api.py
+++ app/routes/api.py
@@ -13,29 +13,17 @@ logger = logging.getLogger(__name__)
 api_routes = Blueprint('api', __name__)
 file_converter = FileConverter()
 
-
-
 @api_routes.route('/files/process', methods=['POST'])
 def create_task():
-    pubSub = PubSubTopic()
     try:
-        pubSub.send_message(request.data.decode())
-
         data = json.loads(request.data)
-
-        pubSub.send_message(data)
 
         message =json.loads(base64.b64decode(data['message']['data']).decode('utf-8'))
 
-        pubSub.send_message(message)
-
         file_converter.converter_request(message["task_id"], message["url"], message["new_format"])
-
-        pubSub.send_message(request.data)
         print(request)
         return request.data, 200
     except Exception as e:
-        pubSub.send_message(e.args[0])
         logger.error("error processing message: error {error}".format(error=e))
         return {"error": e}, 501
 
